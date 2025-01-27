45d9d2b67b81f7755733d0ad80bd1fcdfba7a0c1
Andoni1Barba <Andoni1barba@gmail.com>
2025-01-27 16:16:42 -0400

    first commit learning react

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+function App() {
+  return (
+    <>
+     <article>
+      <header>
+        <img src="https://github.com/Andonny1up.png" alt="El avatar de andoni" />
+        <div>
+          <strong>Andoni Barba Noe</strong>
+          <span>@Andonny1up</span>
+        </div>
+      </header>
+      <aside>
+        <button>
+          seguir
+        </button>
+      </aside>
+     </article>
+    </>
+  )
+}
+
+export default App
