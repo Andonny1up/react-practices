86ccbe8ed3f91f48de8db34195d96a3d67cc4f55
Andonny1up <andoni1barba@gmail.com>
2025-01-29 22:13:54 -0400

    winner

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -24,16 +24,52 @@ const Square = ({children,isSelected,updateBoard, index}) =>{
   )
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) // null = no hay ganador, false empate
+
+  const checkWinner = (boardToCheck) =>{
+    // revisar todas las combinaciones ganadoras
+    for (const combo of WINNER_COMBOS){
+      const [a,b,c] = combo
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
 
   const updateBoard = (index) =>{
+    // no actualizar si ya tiene algo
+    if (board[index] || winner) return 
+    // actualizar tablero
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
+    // cambiar turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    const newWinner = checkWinner(newBoard)
+    if (newWinner){
+      setWinner(newWinner)
+    }
   }
   return (
     <main className='board'>
