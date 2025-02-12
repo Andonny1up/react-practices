379894e7439193627aba4dc5d0ac3c8933b2fda3
Andoni1Barba <Andoni1barba@gmail.com>
2025-02-12 12:45:09 -0400

    redux

--- projects/06-shopping-cart/src/context/cart.jsx
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -1,39 +1,29 @@
-import { createContext,useState } from "react";
+import { createContext,useReducer,useState } from "react";
+import {cartReducer, cartInitialState} from "../reducers/cart";
 
 export const CartContext = createContext()
 
 export function CartProvider({ children }){
-    const [cart , setCart] = useState([])
+    // const [cart , setCart] = useState([])
+    const [state, dispatch]= useReducer(cartReducer, cartInitialState)
 
-    const addToCart = product => {
-        // check if the producto is already in the cart
-        const productInCartIndex = cart.findIndex( item => item.id === product.id)
-    
-        if (productInCartIndex >= 0){
-            const newCart = structuredClone(cart)
-            newCart[productInCartIndex].quantity += 1
-            return setCart(newCart)
-        }
-        // product is not cart
-        setCart(prevState => ([
-            ...prevState,
-            {
-                ...product,
-                quantity: 1
-            }
-        ]))
-    }
-    const removeFromCart = product =>{
-        setCart(prevState => prevState.filter(item => item.id !== product.id))
-    }
-    const clearCart = () =>{
-        setCart([])
-    }
+    const addToCart = product => dispatch({
+        type: 'ADD_TO_CART',
+        payload: product
+    })
+
+    const removeFromCart = product => dispatch({
+        type: 'REMOVE_FROM_CART',
+        payload: product
+    })
+    const clearCart = () => dispatch({
+        type: 'CLEAR_CART'
+    })
 
     return(
         <CartContext.Provider value={
             {
-                cart,
+                cart: state,
                 addToCart,
                 clearCart,
                 removeFromCart
