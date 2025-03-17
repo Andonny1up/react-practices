5b409f10480464b7260cb4d46cd727b545f2cfc6
Andoni1Barba <Andoni1barba@gmail.com>
2025-03-17 16:18:03 -0400

    end router

--- projects/07-router/src/Router.jsx
+++ projects/07-router/src/Router.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState, Children } from 'react'
 import { EVENTS } from './consts'
 import { match } from 'path-to-regexp'
+import { getCurrentPath } from './utils'
 
 export function Router({children, routes = [], defaultComponent: DefaultComponent = () => <h1>404</h1>}) {
-    const [currentPath, setCurrentPath] = useState(window.location.pathname)
+    const [currentPath, setCurrentPath] = useState(getCurrentPath())
   
     useEffect(()=>{
       const onLocationChange = () => {
-        setCurrentPath(window.location.pathname)
+        setCurrentPath(getCurrentPath())
       }
   
       window.addEventListener(EVENTS.PUSHSTATE, onLocationChange)
@@ -29,7 +30,7 @@ export function Router({children, routes = [], defaultComponent: DefaultComponen
       return props
     })
 
-    const routesToUse = routes.concat(routesFromChildren)
+    const routesToUse = routes.concat(routesFromChildren).filter(Boolean)
     
 
     const Page = routesToUse.find(({path}) => {
